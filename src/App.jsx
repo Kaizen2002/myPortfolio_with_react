aea6f4f75b713947f53f6c1a25712e457db0acfc
Kaizen2002 <jyotirmay231@gmail.com>
2024-06-03 21:19:42 +0530

    Start making project section in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Page1 from "./components/Page1";
 import About from "./components/About";
+import Project from "./components/Project";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Page1/>
       <About/>
+      <Project/>
     </>
   )
 }
---
+++ src/components/Project.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Project() {
+    return (
+        <div className='w-full bg-black '>
+            <div className="project-container w-[50%] m-auto flex flex-col ">
+                <div className="text-center text-white tracking-widest text-3xl font-normal mt-10 py-2">PROJECTS</div>
+                <div className=' text-center text-white'>
+                    <p><i>Remember to look</i></p>
+                </div>
+                <div className="projects-list">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
